cb6da9ad31a9c476e022c0ca26720e653de65d6f
Luofan Chen <clfbbn@gmail.com>
2023-03-01 20:51:49 +0800

    sm6375-common: Build lineage health HAL
    Change-Id: I1ca5f25f3a2d8734647b9cc0ff4885b0b817c6ad
    Signed-off-by: althafvly <althafvly@gmail.com>

--- holi.mk
+++ holi.mk
@@ -328,6 +328,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
