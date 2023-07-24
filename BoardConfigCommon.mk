7e08d0e0e82b83474594c6e3dddf148df7bb97d3
boedhack99 <boedhack@gmail.com>
2023-07-24 23:36:34 +0700

    sm6375-common: Update blobs to moonstone MIUI V14.0.4.0.TMPMIXM release-keys
    Change-Id: Ie92837de844c37012d561d869b98245d32b20d75

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -187,7 +187,7 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2023-04-01
+VENDOR_SECURITY_PATCH := 2023-07-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
