0d235d38750381dce022a6900df6028502c0b6b0
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-07-28 18:32:15 +0000

    sm6375-common: Remove RenderScript
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- holi.mk
+++ holi.mk
@@ -364,10 +364,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/telephony_system-ext_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/telephony_system-ext-privapp-permissions-qti.xml
 
-# RenderScript HAL
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-
 # RIL
 PRODUCT_PACKAGES += \
     CarrierConfigOverlay \
