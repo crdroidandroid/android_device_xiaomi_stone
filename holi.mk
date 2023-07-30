8d3c10451114f2a4e6c2c7d5e420902dd42a042e
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-07-30 11:53:37 +0000

    sm6375-common: Switch to common hardware/xiaomi HAL sensors
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- holi.mk
+++ holi.mk
@@ -398,7 +398,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal \
+    android.hardware.sensors@2.1-service.xiaomi-multihal \
     libsensorndkbridge
 
 # Servicetracker
