18269f8388243a9f955a9090ccd32b9f19752d79
Sarthak Roy <sarthakroy2002@gmail.com>
2023-10-22 20:18:58 +0300

    sm6375-common: Switch to Wifi service AIDL
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- device.mk
+++ device.mk
@@ -520,7 +520,7 @@ PRODUCT_FULL_TREBLE_OVERRIDE := true
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
