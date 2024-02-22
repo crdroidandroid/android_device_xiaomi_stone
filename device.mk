49e49ee9c939c46154c73bd4b87738fcd854f35e
Giovanni Ricca <giovanniricca@proton.me>
2024-02-22 20:08:48 +0800

    stone: Build goodix lib from source
    Change-Id: Ibaf1b9489b509318032c720c9c83e315cfe6ab3e
    Signed-off-by: Arijit78 <sahaarijit2007@gmail.com>

--- device.mk
+++ device.mk
@@ -182,7 +182,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
-    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
+    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor \
+    libvendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
 # FM
 PRODUCT_PACKAGES += \
