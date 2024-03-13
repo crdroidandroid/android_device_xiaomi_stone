a882a3fb5b4ea560f8b0689499be949d80781299
AnaadiAnanta <you@example.com>
2024-03-13 17:17:17 +0530

    stone: update security patch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -206,7 +206,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2023-07-01
+VENDOR_SECURITY_PATCH := 2024-02-05
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
