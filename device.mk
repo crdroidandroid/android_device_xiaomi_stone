34d15214737e5086bdcc6d8c6d59a1bc803a133c
kamikaonashi <thorschi93@gmx.at>
2024-05-11 18:23:22 +0200

    stone: inherit Dolby Vendor addon if cloned
    inherit dolby addon from
    https://github.com/cumOS/vendor_dolby_moto
    https://github.com/cumOS/vendor_dolby_oplus

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product-if-exists, vendor/xiaomi/stone-miuicamera/miuicamera.mk)
 # Call the ViperFX Config
 $(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
 
+# Vendor Dolby
+$(call inherit-product-if-exists, vendor/dolby/dolby.mk)
+
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
