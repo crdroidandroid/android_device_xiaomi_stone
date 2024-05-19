0851da0b332a0dcc6e309df5100a9c4f7bca4836
AnaadiAnanta <149001367+AnaadiAnanta@users.noreply.github.com>
2024-05-19 21:50:11 +0530

    stone: Update blobs to moonstone V816.0.5.0.UMPMIXM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -217,7 +217,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2023-07-01
+VENDOR_SECURITY_PATCH := 2024-04-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
