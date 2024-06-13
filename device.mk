4cb97087d3b9c55971b5267aae3816a5cea1923c
Chirayu Desai <chirayudesai1@gmail.com>
2024-06-13 22:02:20 +0530

    stone: Disable UFFD GC
    Unable to determine UFFD GC flag for kernel version "5.4.268-qgki".
    You can fix this by explicitly setting PRODUCT_ENABLE_UFFD_GC to "true" or
    "false" based on the kernel version.
    1. Set PRODUCT_ENABLE_UFFD_GC to "true" if the kernel supports userfaultfd(2)
       and MREMAP_DONTUNMAP.
    2. Set PRODUCT_ENABLE_UFFD_GC to "false" otherwise.
    
    Change-Id: Ie77d9a86a7fe94e4cade1868ffea27194f724e03

--- device.mk
+++ device.mk
@@ -258,6 +258,9 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 # Kernel Headers
 PRODUCT_VENDOR_KERNEL_HEADERS := device/xiaomi/stone-kernel/kernel-headers
 
+# Kernel
+PRODUCT_ENABLE_UFFD_GC := false
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
