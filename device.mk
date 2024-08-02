1b86a7ce49dd9bc03d542acadc2a4034eb19377e
kamikaonashi <thorschi93@gmx.at>
2024-08-02 21:10:03 +0200

    stone: configs: move msm_irqbalance to device tree
    moved from vendor to device tree

--- device.mk
+++ device.mk
@@ -215,6 +215,10 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
+# IRQ
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := false
 
