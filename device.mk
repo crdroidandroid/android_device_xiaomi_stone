0f56b76db12b7b92120ca11e668b5a329e9e7aca
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-06-04 14:33:29 +0530

    stone: Move to Xiaomi IR AIDL
    Change-Id: I46094bcbfaba252b5d98ea8211b927fdaa079e2d

--- device.mk
+++ device.mk
@@ -213,8 +213,7 @@ PRODUCT_PACKAGES += \
 
 # IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
+    android.hardware.ir-service.xiaomi
 
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := false
