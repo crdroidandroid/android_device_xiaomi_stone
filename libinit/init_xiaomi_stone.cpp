d488ead4d99a7a479f282604999e9fbc90f28c85
Vedraj Gawas <gawasvedraj@gmail.com>
2025-10-26 16:29:42 +0530

    stone: libinit: Update fingerprints

--- libinit/init_xiaomi_stone.cpp
+++ libinit/init_xiaomi_stone.cpp
@@ -9,7 +9,7 @@
 #include "vendor_init.h"
 
 void vendor_load_properties() {
-    
+
     const std::vector<variant_info_t> variants = {
         { //MOONSTONE_GLOBAL
             "moonstone_p_global",
@@ -17,7 +17,7 @@ void vendor_load_properties() {
             "moonstone",
             "POCO X5 5G",
             "_blair",
-            "POCO/moonstone_p_global/moonstone:14/UKQ1.231003.002/V816.0.7.0.UMPMIXM:user/release-keys"
+            "POCO/moonstone_p_global/moonstone:14/UKQ1.231003.002/V816.0.22.0.UMPMIXM:user/release-keys"
         },
         { //MOONSTONE_IN
             "moonstone_p_in",
@@ -25,7 +25,7 @@ void vendor_load_properties() {
             "moonstone",
             "POCO X5 5G",
             "_blair",
-            "POCO/moonstone_p_in/moonstone:14/UKQ1.231003.002/V816.0.5.0.UMPINXM:user/release-keys"
+            "POCO/moonstone_p_in/moonstone:14/UKQ1.231003.002/V816.0.19.0.UMPINXM:user/release-keys"
         },
         { //SUNSTONE_CN
             "sunstone_cn",
@@ -33,7 +33,7 @@ void vendor_load_properties() {
             "sunstone",
             "Redmi Note 12 5G",
             "_blair_lite",
-            "Redmi/sunstone/sunstone:14/UKQ1.230917.001/V816.0.2.0.UMQCNXM:user/release-keys"
+            "Redmi/sunstone/sunstone:14/UKQ1.240624.001/OS2.0.6.0.UMQCNXM:user/release-keys"
         },
         { //SUNSTONE_GLOBAL
             "sunstone_global",
@@ -41,7 +41,7 @@ void vendor_load_properties() {
             "sunstone",
             "Redmi Note 12 5G",
             "_blair_lite",
-            "Redmi/sunstone_global/sunstone:14/UKQ1.240624.001/OS2.0.1.0.UMQMIXM:user/release-keys"
+            "Redmi/sunstone_global/sunstone:14/UKQ1.240624.001/OS2.0.4.0.UMQMIXM:user/release-keys"
         },
         { //SUNSTONE_IN
             "sunstone_in",
@@ -49,7 +49,7 @@ void vendor_load_properties() {
             "sunstone",
             "Redmi Note 12 5G",
             "_blair_lite",
-            "Redmi/sunstone_in/sunstone:14/UKQ1.230917.001/V816.0.4.0.UMQINXM:user/release-keys"
+            "Redmi/sunstone_in/sunstone:14/UKQ1.240624.001/OS2.0.6.0.UMQINXM:user/release-keys"
         },
         { //SUNSTONE_X
             "sunstone_x",
@@ -57,10 +57,10 @@ void vendor_load_properties() {
             "sunstone",
             "Redmi Note 12R Pro",
             "_blair_lite",
-            "Redmi/sunstone/sunstone:14/UKQ1.230917.001/V816.0.2.0.UMQCNXM:user/release-keys"
+            "Redmi/sunstone/sunstone:14/UKQ1.240624.001/OS2.0.6.0.UMQCNXM:user/release-keys"
         }
     };
-    
+
     search_variant(variants);
     set_dalvik_heap();
 }
