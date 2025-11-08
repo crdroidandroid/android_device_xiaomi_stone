9272fcaa78440c9d793594b8a33d8c190343cdf0
Dede Dindin Qudsy <xtrymind@gmail.com>
2025-11-09 03:48:12 +0530

    stone: Setup `HWUI_COMPILE_FOR_PERF` flag
    This will build framework with :
    
    cflags: [
        "-fno-omit-frame-pointer",
        "-marm",
        "-mapcs",
    ]
    
    Change-Id: I887f7f91a960f579ef2ee39a534966dad3bac23f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,6 +65,9 @@ DEVICE_MANIFEST_FILE := \
 DEVICE_MATRIX_FILE := \
     hardware/qcom-caf/common/compatibility_matrix.xml
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image
