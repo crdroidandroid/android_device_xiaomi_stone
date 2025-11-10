9f02da86b7f76722dc2236ce078a038351290c08
bugsoverloaded <193326294+bugsoverloaded@users.noreply.github.com>
2025-11-10 17:31:33 +0530

    stone: Update blobs to OS2.0.5.0.UMQMIXM

--- lineage_stone.mk
+++ lineage_stone.mk
@@ -30,6 +30,6 @@ PRODUCT_NAME := lineage_stone
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="sunstone_global-user 14 UKQ1.240624.001 OS2.0.1.0.UMQMIXM release-keys" \
-    BuildFingerprint=Redmi/sunstone_global/sunstone:14/UKQ1.240624.001/OS2.0.1.0.UMQMIXM:user/release-keys \
+    BuildDesc="sunstone_global-user 14 UKQ1.240624.001 OS2.0.5.0.UMQMIXM release-keys" \
+    BuildFingerprint=Redmi/sunstone_global/sunstone:14/UKQ1.240624.001/OS2.0.5.0.UMQMIXM:user/release-keys \
     DeviceProduct=sunstone
