bc1ce27eb006697515c824154ce05f4c30e01d18
kamikaonashi <thorschi93@gmx.at>
2025-12-10 17:40:44 +0100

    stone: add FCM from hardware/xiaomi

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,6 +57,7 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
