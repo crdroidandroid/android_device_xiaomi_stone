a7a0f82ecbe87942d08589fe5d537ad0b393ba1d
kamikaonashi <thorschi93@gmx.at>
2025-12-23 09:58:37 +0100

    stone: overlay: add secure NFC for supported variants

--- device.mk
+++ device.mk
@@ -277,7 +277,8 @@ PRODUCT_PACKAGES += \
     LauncherOverlayTarget \
     SettingsOverlayTarget \
     SystemUIOverlayTarget \
-    WifiOverlayTarget
+    WifiOverlayTarget \
+    NfcOverlayTarget
 
 PRODUCT_PACKAGES += \
     FrameworkOverlayMoonstone \
@@ -391,7 +392,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     charger_fstab.qti \
     fstab.default
-  
+
 PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.kernel.post_boot.sh \
