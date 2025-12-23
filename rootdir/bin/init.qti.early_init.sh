3b40e445c0c1a2aa3eff9a63cb40feaa09892224
kamikaonashi <thorschi93@gmx.at>
2025-12-23 22:59:27 +0100

    stone: rootdir: cleanup init.qti.early_init.sh

--- rootdir/bin/init.qti.early_init.sh
+++ rootdir/bin/init.qti.early_init.sh
@@ -33,15 +33,13 @@
 # IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-soc_id=`cat /sys/devices/soc0/soc_id` 2> /dev/null
+soc_id="$(cat /sys/devices/soc0/soc_id 2>/dev/null)"
 
 # Store soc_id in ro.vendor.qti.soc_id
-setprop ro.vendor.qti.soc_id $soc_id
+[ -n "$soc_id" ] && setprop ro.vendor.qti.soc_id "$soc_id"
 
-if [ "$soc_id" -eq 507 ]; then
-    setprop ro.vendor.qti.soc_model SM6375
-elif [ "$soc_id" -eq 454 ]; then
-    setprop ro.vendor.qti.soc_model SM4350
-elif [ "$soc_id" -eq 578 ]; then
-    setprop ro.vendor.qti.soc_model SM4375
-fi
+case "$soc_id" in
+  507) setprop ro.vendor.qti.soc_model SM6375 ;;
+  578) setprop ro.vendor.qti.soc_model SM4375 ;;
+  *) ;;
+esac
